108e6f7a663e6fd985001a43c33d80df0765f47a
Shrayash Shukla <ishrayash@gmail.com>
2024-08-10 14:45:47 +0530

    10-8-2024

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose, { get } from "mongoose";
+import cors from "cors";
+import env from "dotenv";
+
+import { get404 } from "./controllers/error.js";
+
+import mailRouter from "./routes/mail.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+env.config();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(mailRouter);
+app.use(get404);
+
+mongoose
+  .connect(
+    "mongodb+srv://ishu:lNwKH7FlCS8wwZBx@cluster0.bbugwp2.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((result) => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+      console.log("Connected to Database");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
