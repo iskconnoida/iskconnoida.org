7d3b169d8a2b8a86e5ecd7f44b2da2eac5a3ec15
Dhruv Trivedi <95343534+drvcodenta@users.noreply.github.com>
2024-08-11 16:42:44 +0530

    responsive temple schedule button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import TempleScheduleModal from "./TempleScheduleModal";
 
 export default function Hero() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+      setModalOpen(true);
+  };
+
+  const closeModal = () => {
+      setModalOpen(false);
+  };
   let imgURL: string = "";
   switch (new Date().getUTCDay()) {
     // uncalibrated placeholder images, subject to change
@@ -42,14 +52,14 @@ export default function Hero() {
   return (
     <>
       <div id="hero" className="lg:mt-[3.5rem] h-[calc(100vh-3.5rem)] flex flex-col w-screen bg-center bg-no-repeat bg-cover text-center">
-        <h1 className="text-4xl font-bold text-rose-100 text-shadow-xl shadow-red-300 flex  justify-center items-end mt-auto py-6 px-4 h-1/2 w-full">
+        <h1 className="text-4xl font-bold text-rose-100 text-shadow-xl shadow-red-300 flex  justify-center items-end mt-auto py-6 px-4 h-1/2 w-full ">
           WELCOME TO SRI SRI RADHA GOVIND DEV JI TEMPLE
         </h1>
         {/* <div className="bg-gradient-to-b from-transparent to-gray-800"> */}
         <div className="relative w-full h-2/6 flex justify-center items-center">
         <div className="absolute bottom-0 left-0 h-4/5 right-0 bg-gradient-to-b from-transparent to-white z-0"></div>
         <div className="bg-white sm:w-2/3 w-11/12 h-full rounded-t-2xl z-10">
-        <div className="flex flex-col sm:flex-row items-center justify-center mt-4 space-x-2 sm:space-y-0 sm:space-x-2">
+        <div className="flex flex-row items-center justify-center mt-4 space-x-2 sm:space-y-0 sm:space-x-2">
           <span className="bg-green-500 w-3 h-3 rounded-full animate-pulse"></span>
           <span className="text-lg sm:text-2xl text-center sm:text-left font-semibold">OPEN - Ongoing Rajbhog Arati</span>
         </div>
@@ -58,8 +68,9 @@ export default function Hero() {
           <p className="">4:15 pm - 9 pm IST</p>
         </div>
         <div className="text-center mt-6">
-          <button className="border border-violet-700 rounded-lg bg-violet-600 px-4 py-2 text-white shadow-lg shadow-violet-300 hover:shadow-violet-300 hover:shadow-xl">View Temple Schedule</button>
+          <button onClick={openModal} className="border border-violet-500 rounded-lg bg-violet-500 px-4 py-2 text-white shadow-lg shadow-violet-300 hover:shadow-violet-300 hover:shadow-xl">View Temple Schedule</button>
         </div>
+        <TempleScheduleModal isOpen={isModalOpen} onClose={closeModal} />
         </div>
         </div>
         
