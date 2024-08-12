f4febec2c42349ebdf25a6650a325e45c721d971
Dhruv Trivedi <95343534+drvcodenta@users.noreply.github.com>
2024-08-12 18:32:24 +0530

    working background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -80,7 +80,7 @@ const Navbar: React.FC = () => {
         <Link
           to="/events"
           className={`md:text-base lg:text-xl font-semibold text-gray-600 rounded-full px-2 py-1 text-shadow-lg shadow-purple-100 ${
-            isActive("/festivals") ? "bg-gray-600 text-white" : "bg-white"
+            isActive("/events") ? "bg-gray-600 text-white" : "bg-white"
           }`}
         >
           Festivals
