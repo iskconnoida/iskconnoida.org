cf1087757c3731e555f20fcfa9c03810a285aaae
Dhruv Krishna Vaid <dhruvkrishnavaid@gmail.com>
2024-08-19 22:28:17 +0530

    Use a centralized source of data (utils/handler.ts)

--- frontend/src/components/Donate.tsx
+++ frontend/src/components/Donate.tsx
@@ -1,48 +1,8 @@
 import { Link } from "react-router-dom";
 import Card from "./Card";
-
-type Response = Record<
-  string,
-  {
-    id: string;
-    title: string;
-    img: string;
-    description: string;
-    donation_link: string;
-  }
->;
+import { donations } from "../utils/handler";
 
 export default function Donate() {
-  const res: Response = {
-    services_response1: {
-      id: "12",
-      title: "Anna Daan 2024",
-      img: "upload/catimg/37693cfc748049e45d87b8c7d8b9aacd.Annadaan%201.png",
-      description:
-        "Anna daan is the act of donating food to the needy. In Vedic culture, it is considered one of the most pious and virtuous acts. Anna Daan is considered to be Mahadaan. It is believed that offering food to others is equivalent to offering it to God Himself.",
-      donation_link: "https://iic.iskconnoida.org/donate/Anna_Daan_2024",
-    },
-    services_response2: {
-      id: "11",
-      title: "Shri Krishna Janmashtami 2024",
-      img: "upload/catimg/f899139df5e1059396431415e770c6dd.janmashtami 2024_11zon.webp",
-      description:
-        "On the most auspicious occasion of 5251st Janmashtami Mahotsava of Lord Shri Krishna, may the Lordships Shri Shri Radha Govind Dev shower their love in your hearts. Feel free to donate on the occasion of Shri Krishna Janmashtami and be a part of this festivity. Hare Krishna.",
-      donation_link:
-        "https://iic.iskconnoida.org/donate/SHRI_KRISHNA_JANMASHTAMI_2024,_NOIDA",
-    },
-  };
-
-  const events: {
-    id: string;
-    title: string;
-    img: string;
-    description: string;
-    donation_link: string;
-  }[] = [];
-  for (const e in res) {
-    events.push(res[e]);
-  }
   return (
     <>
       <div className="hidden px-12 sm:flex w-full">
@@ -57,15 +17,15 @@ export default function Donate() {
         Donate
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {events.map((event) => (
+        {donations.map((donate) => (
           <Card
-            key={event.id}
-            img={"https://iic.iskconnoida.org/" + event.img}
+            key={donate.id}
+            img={"https://iic.iskconnoida.org/" + donate.img}
             dateless
-            title={event.title}
-            donate={event.donation_link}
+            title={donate.title}
+            donate={donate.donation_link!}
           >
-            {event.description}
+            {donate.description}
           </Card>
         ))}
       </div>
--- frontend/src/pages/Facilities.tsx
+++ frontend/src/pages/Facilities.tsx
@@ -1,64 +1,10 @@
 import { Link } from "react-router-dom";
 import Card from "../components/Card";
 import { motion } from "framer-motion";
+import { facilities } from "../utils/handler";
 
-interface FacilitiesProps {
-  onHomePage: boolean;
-}
-
-type Response = Record<
-  string,
-  {
-    id: string;
-    title: string;
-    img: string;
-    description: string;
-  }
->;
-
-const res: Response = {
-  facilities_response1: {
-    id: "13",
-    title: "Guest House",
-    img: "/images/static-_1665002df6dabb1-Edited-scaled.webp",
-    description:
-      "Stay at ISKCON's BACK TO HOME Guest houses, get a chance to live close to temple and spend the time of your stay in complete Krishna Consciousness.",
-  },
-  facilities_response2: {
-    id: "14",
-    title: "Krishna Expo",
-    img: "/images/1780066_10151922662621364_1967521465_o.webp",
-    description:
-      "Explore Krishna's Leelas presented in a beautiful form, that is attractive to all.",
-  },
-  facilities_response3: {
-    id: "15",
-    title: "Function Halls",
-    img: "/images/umage-1024x682-Edited.webp",
-    description:
-      "Celebrate your Family and Corporate functions with the auspicious blessings of Sri Sri Radha Govind Devji.",
-  },
-  facilities_response4: {
-    id: "16",
-    title: "Govindas Restaurant",
-    img: "/images/download-10.webp",
-    description:
-      "Dine a Pure Veg meal at the world famous ISKCON Govindas Restaurant and Bakery. Everything here from cakes and pastries to pastas and main course is Pure veg.",
-  },
-};
-
-const facilities: {
-  id: string;
-  title: string;
-  img: string;
-  description: string;
-}[] = [];
-for (const facility in res) {
-  facilities.push(res[facility]);
-}
-
-export default function Facilities({ onHomePage }: FacilitiesProps) {
-  return onHomePage ? (
+export default function Facilities(props: { onHomePage: boolean }) {
+  return props.onHomePage ? (
     <>
       <div className="hidden px-12 sm:flex w-full">
         <div className="w-1/2 text-3xl font-bold py-14">Temple Facilities</div>
