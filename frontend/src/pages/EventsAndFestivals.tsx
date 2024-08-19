e85c80bbec6c16246b1e78e3439f3134e14ebaf9
Dhruv Krishna Vaid <69188774+dhruvkrishnavaid@users.noreply.github.com>
2024-08-19 17:02:53 +0530

    Update EventsAndFestivals.tsx

--- frontend/src/pages/EventsAndFestivals.tsx
+++ frontend/src/pages/EventsAndFestivals.tsx
@@ -20,7 +20,7 @@ export default function EventsAndFestivals() {
       title: "Shri Krishna Janmashtami 2024",
       img: "upload/catimg/6ea9ab1baa0efb9e19094440c317e21b.29_08_2021-krishna_30_2_21972025.jpg",
       description:
-        "Krishna Janmashtami, also known simply as Krishnashtami, Janmashtami, or Gokulashtami, is an annual Hindu festival that celebrates the birth of Krishna, the eighth avatar of Vishnu. In certain Hindu texts, such as the Gita Govinda, Krishna has been identified as supreme God and the source of all avatars.",
+        "Hare Krishna",
       date: "2024-08-26",
     },
   };
